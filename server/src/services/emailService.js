11d8c687e84f817bc84dd3aa31d2d4105805f12c
Gokulraj <gokulraj.gokul3003@gmail.com>
2026-06-18 02:36:43 +0530

    Production ready - Kuriosity v1.0

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -1,7 +1,10 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -13,6 +16,8 @@ function generateOTP() {
 }
 
 async function sendOTP(email, otp) {
+    console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('Sending OTP to:', email);
   const mailOptions = {
     from: `"Kuriosity" <${process.env.EMAIL_USER}>`,
     to: email,
